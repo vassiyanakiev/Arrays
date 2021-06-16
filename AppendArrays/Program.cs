74b06c30dbafb5eee6972b5e77df87175cdddd8d
Vasilena Georgieva <75674763+vassiyanakiev@users.noreply.github.com>
2021-06-16 15:14:23 +0300

    Append Arrays

---
+++ AppendArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrays = Console.ReadLine()
+               .Split('|', StringSplitOptions.RemoveEmptyEntries)
+               .ToArray();
+
+            List<string> result = new List<string>();
+
+            for (int i = arrays.Length - 1; i >= 0; i--)
+            {
+                string[] elements = arrays[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                result.AddRange(elements);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
