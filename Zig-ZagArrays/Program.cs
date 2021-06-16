c5664c47e8df5c174c6e46b5e7dd99ba3104ba32
Vasilena Georgieva <75674763+vassiyanakiev@users.noreply.github.com>
2021-06-16 15:17:37 +0300

    Zig-Zag Arrays

---
+++ Zig-ZagArrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+namespace Zig_ZagArrays
+{
+    class Program
+    {
+        static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int[] arr1 = new int[size];
+
+            int[] arr2 = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                int[] numbers = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int firstNumber = numbers[0];
+                int secondNumber = numbers[1];
+
+                if (i % 2 != 0)
+                {
+                    arr1[i] = secondNumber;
+                    arr2[i] = firstNumber;
+                }
+                else
+                {
+                    arr1[i] = firstNumber;
+                    arr2[i] = secondNumber;
+                }
+            }
+            Console.WriteLine(string.Join(" ", arr1));
+            Console.WriteLine(string.Join(" ", arr2));
+        }
+    }
+}
