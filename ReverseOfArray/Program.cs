5cf9f812d73168525a48c3ef6482bc9028fd1c23
Vasilena Georgieva <75674763+vassiyanakiev@users.noreply.github.com>
2021-06-16 15:19:55 +0300

    ReverseArray

---
+++ ReverseOfArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array size: ");
+            int size = int.Parse(Console.ReadLine());
+            int[] arr = new int[size];
+
+
+            Console.WriteLine($"Enter {size} element in array:");
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Array in reverse: ");
+            for (int i = size - 1; i >= 0; i--)
+            {
+                Console.Write("{0} ", arr[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
